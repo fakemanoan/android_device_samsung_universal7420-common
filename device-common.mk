fbbc8be161782e589286d2ea74d590f688e54043
enesuzun2002 <enesuzun200227@gmail.com>
2020-05-12 21:21:59 +0300

    universal7420: build graphics packages
    Change-Id: I8db0fefa984e5d2fd6681d7db50301be6d98374f

--- device-common.mk
+++ device-common.mk
@@ -46,7 +46,14 @@ PRODUCT_PACKAGES += \
 # Configstore
 PRODUCT_PACKAGES += \
     android.hardware.configstore@1.0-impl \
-    android.hardware.configstore@1.0-service    
+    android.hardware.configstore@1.0-service
+    
+# Graphics
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl
 
 # Permissions
 PRODUCT_COPY_FILES += \
