a32da7e1308a0086e61f3609b039c3c5f8e5e2fc
Dominggoes Isakh <drjisakh@gmail.com>
2021-01-07 08:35:26 +0100

    universal7420: Replace BOARD_CHARGER_ENABLE_SUSPEND with ro.charger.enable_suspend

--- system_prop.mk
+++ system_prop.mk
@@ -20,8 +20,11 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.chipname=exynos7420 \
 	ro.arch=exynos7420
-	
-	
+
+# Charger
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.charger.enable_suspend=true
+
 # SoC
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.soc.manufacturer=Samsung \
