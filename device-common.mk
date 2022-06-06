25aa308a83383230ccd47c00b67c5319b6e3c38a
kde <55951275+fakemanoan@users.noreply.github.com>
2022-06-06 08:42:31 +0100

    disable configstore

--- device-common.mk
+++ device-common.mk
@@ -95,10 +95,9 @@ PRODUCT_PACKAGES += \
 	android.hardware.camera.provider@2.4-impl \
 	android.hardware.camera.provider@2.4-service
 
-# Configstore
+# ConfigStore
 PRODUCT_PACKAGES += \
-	android.hardware.configstore@1.0-impl \
-	android.hardware.configstore@1.0-service
+    disable_configstore
 
 # Debugging
 -include $(LOCAL_PATH)/system_prop_debug.mk
@@ -107,7 +106,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
 	android.hardware.drm@1.0-impl \
 	android.hardware.drm@1.0-service \
-	android.hardware.drm@1.3-service.clearkey
+	android.hardware.drm@1.4-service.clearkey
 
 # DTB
 PRODUCT_HOST_PACKAGES += \
