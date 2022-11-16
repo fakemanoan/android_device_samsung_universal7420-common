6dcc4c12f75a77471bb6793519ea524a1f05a851
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-11-16 14:58:41 +0000

    update media stuff
    Change-Id: I7d0b0e6e14660fb34ad81d3a14f25479b20db335

--- device-common.mk
+++ device-common.mk
@@ -200,7 +200,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/media/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
+	frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
 # NFC
 PRODUCT_PACKAGES += \
@@ -215,7 +215,8 @@ PRODUCT_PACKAGES += \
 # OMX
 PRODUCT_PACKAGES += \
 	android.hardware.media.omx@1.0-impl \
-	android.hardware.media.omx@1.0-service
+	android.hardware.media.omx@1.0-service \
+	mediaserver.rc
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
