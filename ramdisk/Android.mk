8f969b6b8c2d2ef1e66131886f3e42a326bb948c
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-11-20 15:08:21 +0000

    Update Android.mk
    Change-Id: Ie8b0e1dcfc8c837beb297e916564423d62805715

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -1,4 +1,39 @@
 LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= fstab.samsungexynos7420
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= fstab.samsungexynos7420
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.baseband.rc
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.baseband.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.gps.rc
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.gps.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE            := init.zerofltexx.rc
+LOCAL_MODULE_CLASS      := ETC
+LOCAL_SRC_FILES         := init.zerofltexx.rc
+LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE		:= ueventd.samsungexynos7420.rc
+LOCAL_MODULE_STEM  := ueventd.rc
+LOCAL_MODULE_CLASS	:= ETC
+LOCAL_SRC_FILES		:= ueventd.samsungexynos7420.rc
+LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE            := init.battery.rc
@@ -35,31 +70,3 @@ LOCAL_SRC_FILES		:= init.samsungexynos7420.usb.rc
 LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= fstab.samsungexynos7420
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= fstab.samsungexynos7420
-LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.baseband.rc
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := init.baseband.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE            := init.gps.rc
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := init.gps.rc
-LOCAL_MODULE_PATH       := $(TARGET_OUT_VENDOR_ETC)/init/hw
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE		:= ueventd.samsungexynos7420.rc
-LOCAL_MODULE_STEM	:= ueventd.rc
-LOCAL_MODULE_CLASS	:= ETC
-LOCAL_SRC_FILES		:= ueventd.samsungexynos7420.rc
-LOCAL_MODULE_PATH	:= $(TARGET_OUT_VENDOR_ETC)
-include $(BUILD_PREBUILT)
