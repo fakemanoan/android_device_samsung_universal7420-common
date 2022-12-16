9d3e71da8c49268b1f2642e4bcbe6d7f92bbaf90
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-12-16 21:50:46 +0000

    universal7420: build only 32bit audio hal

--- device-common.mk
+++ device-common.mk
@@ -21,10 +21,10 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default \
     audio.usb.default \
     audio.r_submix.default \
-    audio.primary.universal7420 \
-    android.hardware.audio@2.0-impl \
+    audio.primary.universal7420_32 \
+    android.hardware.audio@2.0-impl:32 \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-impl:32 \
     android.hardware.audio.effect@2.0-service \
     libtinycompress
 
