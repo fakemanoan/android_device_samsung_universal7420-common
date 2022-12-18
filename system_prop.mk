c5b0ab4a4aaddef105254c72b1d34b92a40d814f
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-12-18 23:04:37 +0000

    universal7420: Fix rank of OMX audio codec components
    Rank OMX SW audio codecs higher than C2 codecs.

--- system_prop.mk
+++ system_prop.mk
@@ -47,7 +47,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	
 # OMX
 PRODUCT_PROPERTY_OVERRIDES += \
-	debug.stagefright.ccodec=0
+	debug.stagefright.ccodec=0 \
+	debug.stagefright.omx_default_rank=0 \
+	debug.stagefright.omx_default_rank.sw-audio=1 
 	
 # Vendor Security Patch Level
 PRODUCT_PROPERTY_OVERRIDES += \
