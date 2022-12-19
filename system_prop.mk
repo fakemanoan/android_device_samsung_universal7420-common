786512e422989ff10a089673b78c1498ba636b3e
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-12-19 18:13:28 +0000

    universal7420: enable dex2oat64

--- system_prop.mk
+++ system_prop.mk
@@ -33,7 +33,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	dalvik.vm.heapsize=512m \
 	dalvik.vm.heaptargetutilization=0.75 \
 	dalvik.vm.heapminfree=512k \
-	dalvik.vm.heapmaxfree=8m
+	dalvik.vm.heapmaxfree=8m \
+	dalvik.vm.dex2oat64.enabled=true
 	
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
