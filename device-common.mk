b025287f2f921a67dca1538570421cd806963ecd
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2022-12-22 12:37:41 +0000

    universal7420: add boot animation stuff

--- device-common.mk
+++ device-common.mk
@@ -56,6 +56,13 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
     libbt-vendor
+
+# Boot animation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 2560
+TARGET_SCREEN_WIDTH := 1440
     
 # Camera
 PRODUCT_PACKAGES += \
