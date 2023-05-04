18c510b982b85f562820c7ff8bc3e52d3e7f4d33
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-05-04 16:04:14 +0100

    Update system_prop to coincide with new linaro bsp

--- system_prop.mk
+++ system_prop.mk
@@ -75,7 +75,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # HWC
 PRODUCT_PROPERTY_OVERRIDES += \
 	debug.sf.latch_unsignaled=1 \
-	debug.hwc.winupdate=1
+	ro.vendor.winupdate=1
 	debug.sf.disable_backpressure=1 \
 	ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
