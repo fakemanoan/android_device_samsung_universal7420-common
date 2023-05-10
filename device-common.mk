fcuzzocrea/android_device_samsung_universal8890-common

universal8890: add device specific atrace HAL

device-common.mk
@@ -48,6 +48,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/ld.config.txt:$(TARGET_COPY_OUT_VENDOR)/etc/swcodec/ld.config.txt
 
+# Atrace HAL
+PRODUCT_PACKAGES += \
+    android.hardware.atrace@1.0-service.universal8890
+
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \

fakemanoan/android_device_samsung_universal7420-common

Change-Id: I4e9154b883f0cce55d85a8b90d34f23c426199b2

device-common.mk
@@ -72,6 +72,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
 	AdvancedDisplay
 
+# Atrace HAL
+PRODUCT_PACKAGES += \
+	android.hardware.atrace@1.0-service.universal7420
+
 # Audio
 PRODUCT_PACKAGES += \
 	audio.primary.universal7420_32 \