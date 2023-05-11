a9335d7b92233ae19b18a5c140104caa7733ced0
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2023-05-11 16:04:20 +0100

    Swap to 2.1 ver of mapper

--- device-common.mk
+++ device-common.mk
@@ -180,7 +180,7 @@ PRODUCT_PACKAGES += \
 	android.hardware.graphics.allocator@2.0-impl \
 	android.hardware.graphics.allocator@2.0-service \
 	android.hardware.graphics.composer@2.1-service \
-	android.hardware.graphics.mapper@2.0-impl \
+	android.hardware.graphics.mapper@2.0-impl-2.1 \
 	libhwc2on1adapter
 
 PRODUCT_COPY_FILES += \
